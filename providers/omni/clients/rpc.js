22e910ed76c1d857b2bfc457beb0fc571e99270d
ivan <vanzay84@gmail.com>
2018-12-14 13:37:33 +0500

    omni

---
+++ providers/omni/clients/rpc.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const BitcoinClient = require('bitcoin-core');
+const url = require('url');
+
+module.exports = (addr, timeout = 30 * 1000, logger) => {
+  let url_obj = url.parse(addr);
+  let client = new BitcoinClient({
+    host: url_obj.hostname,
+    port: url_obj.port,
+    username: url_obj.auth.split(':')[0],
+    password: url_obj.auth.split(':')[1],
+    timeout
+  });
+  return {
+    async cmd(command, ...args) {
+      return new Promise((resolve, reject) => client.command(command, ...args, (err, result) => err ? reject(err) : resolve(result)));
+    },
+
+    async getCurrentHeight() {
+      let info = await this.cmd('omni_getinfo');
+      return info.block;
+    },
+
+    async getPool() {
+      return this.cmd('omni_listpendingtransactions');
+    },
+
+    async getBlock(height) {
+      let blockhash = await this.cmd('getblockhash', height);
+      let block = await this.cmd('getblock', blockhash, true);
+      let omniTxs = await this.cmd('omni_listblocktransactions', height);
+      let txs = [];
+      for (let txid of omniTxs) {
+        txs.push({
+          txid,
+          blockhash,
+          confirmations: block.confirmations, // TODO it's different for btc and omni
+          time: block.time,
+          blocktime: block.time
+        });
+      }
+      return {
+        height,
+        hash: blockhash,
+        prev_hash: block.previousblockhash,
+        txs
+      }
+    }
+  }
+};
