49a030e5b7f604b6d183a595bd1e4ccb804bd28b
ivan <vanzay84@gmail.com>
2019-04-26 10:57:28 +0500

    eos

---
+++ providers/eos/clients/rpc.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const {JsonRpc} = require('eosjs');
+const fetch = require('node-fetch');
+
+module.exports = (addr, logger) => {
+  const rpc = new JsonRpc(addr, {fetch});
+
+  return {
+    rpc,
+
+    async getCurrentHeight() {
+      let info = await rpc.get_info();
+      return info.last_irreversible_block_num;
+    },
+
+    async getPool() {
+      return []
+    },
+
+    async getBlock(height) {
+      let block = await rpc.get_block(height);
+      return {
+        height: block.block_num,
+        hash: block.id,
+        prev_hash: block.previous,
+        txs: block.transactions
+          .filter(t => t.status === 'executed')
+          .map(t => t.trx)
+      }
+    }
+  }
+};
