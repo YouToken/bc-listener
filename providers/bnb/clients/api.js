2918b174a1cc7a9c548e0fb3f36aa35ad0ba742c
ivan <vanzay84@gmail.com>
2019-10-15 16:23:24 +0200

    binance api rate limits

--- providers/bnb/clients/api.js
+++ providers/bnb/clients/api.js
@@ -4,16 +4,39 @@ const request = require('superagent');
 
 module.exports = (addr) => {
   return {
+    timeLimit: {},
+
     async getAccount(account) {
-      // TODO Rate Limit: 5 requests per IP per second.
-      return request.get(`${addr}/api/v1/account/${account}`)
+      return this._execute('getAccount', 5, request.get, `${addr}/api/v1/account/${account}`)
         .then(response => response.body);
     },
 
     async getAccountSequence(account) {
-      // TODO Rate Limit: 5 requests per IP per second.
-      return request.get(`${addr}/api/v1/account/${account}/sequence`)
+      return this._execute('getAccountSequence', 5, request.get, `${addr}/api/v1/account/${account}/sequence`)
         .then(response => response.body.sequence);
+    },
+
+    async _execute(key, rateLimit, fn, ...args) {
+      return new Promise((resolve, reject) => {
+        let now = new Date().getTime();
+        this.timeLimit[key] = this.timeLimit[key] || now;
+        let timeout = 0;
+        if (this.timeLimit[key] > now) {
+          timeout = this.timeLimit[key] - now;
+          this.timeLimit[key] += rateLimit * 1000;
+        } else {
+          this.timeLimit[key] = now + rateLimit * 1000;
+        }
+        setTimeout(() => {
+          fn(...args)
+            .then(data => {
+              resolve(data);
+            })
+            .catch(e => {
+              reject(e);
+            });
+        }, timeout);
+      });
     }
   }
 };
