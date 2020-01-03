eef36d2aacd4a808c1f7fef2555db9b141e257d2
ivan <vanzay84@gmail.com>
2020-01-03 19:14:14 +0500

    refactoring

---
+++ index.js
@@ -0,0 +1,9 @@
+const Listener = require('./listener');
+const AsyncListener = require('./asyncListener');
+const AddressListener = require('./addressListener');
+
+module.exports = {
+  Listener,
+  AsyncListener,
+  AddressListener
+};
