dfadd02a42f6a70dca3effd67980a4c302d1f63a
ivan <vanzay84@gmail.com>
2020-06-25 15:57:37 +0500

    blockchair.com balance

--- providers/btc/clients/blockchair.com.js
+++ providers/btc/clients/blockchair.com.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const request = require('superagent');
+const BigNumber = require('bignumber.js');
 
 module.exports = (conf = {}) => {
   const NETWORK = conf.network;
@@ -12,9 +13,20 @@ module.exports = (conf = {}) => {
           .get(`https://api.blockchair.com/${NETWORK}/stats`)
           .then(response => response.body.data.blocks);
       }
+      if (command === 'getbalance') {
+        return await request
+          .get(`https://api.blockchair.com/${NETWORK}/addresses/balances?addresses=${args[0]}`)
+          .then(response => response.body.data[args[0]]);
+      }
     },
+
     async getCurrentHeight() {
       return this.cmd('getblockcount');
+    },
+
+    async getBalance(address) {
+      let balance = await this.cmd('getbalance', address);
+      return new BigNumber(balance).div('100000000');
     }
   }
 };
