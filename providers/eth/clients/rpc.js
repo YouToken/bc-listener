bacd85f376603295055f26878f01a12d842478a3
ivan <vanzay84@gmail.com>
2021-02-17 15:16:32 +0500

    binance smart chain support

--- providers/eth/clients/rpc.js
+++ providers/eth/clients/rpc.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Web3 = require('web3');
+const formatters = require('web3-core-helpers').formatters;
 const util = require('util');
 const BigNumber = require('bignumber.js');
 
@@ -21,12 +22,33 @@ module.exports = class EthereumRpc {
     });
   }
 
+  async _rpcCall(method, params) {
+    return new Promise((resolve, reject) => {
+      this.web3.currentProvider.send({
+        jsonrpc: "2.0",
+        method,
+        params,
+        id: new Date().getTime()
+      }, function (err, response) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(response.result);
+        }
+      });
+    });
+  }
+
   async cmd(command, ...args) {
     if (command === 'eth_blockNumber') {
       return this.eth.getBlockNumber();
     }
     if (command === 'eth_getBlockByNumber') {
-      let data = await this.eth.getBlock(+args[0], true);
+      // let data = await this.eth.getBlock(+args[0], true);
+      // get block by raw rpc call because of https://github.com/binance-chain/bsc/issues/44
+      let block = await this._rpcCall("eth_getBlockByNumber", [formatters.inputBlockNumberFormatter(+args[0]), true]);
+      block.transactions = block.transactions.filter(tx => tx.to !== "0x0000000000000000000000000000000000001000");
+      let data = formatters.outputBlockFormatter(block);
       for (let tx of data.transactions) {
         tx.receipt = await this.cmd('eth_getTransactionReceipt', tx.hash);
         tx.gasPrice = (new BigNumber(tx.gasPrice)).toString(10);
